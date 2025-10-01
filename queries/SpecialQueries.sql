2e0b9639606c8e57c45d2e8b60533d159c1283a5
User-TK <tommytkerr@gmail.com>
2025-10-01 16:43:37 -0500

    Add Q3
    Adds the Q3 query

--- queries/SpecialQueries.sql
+++ queries/SpecialQueries.sql
@@ -104,4 +104,6 @@ LEFT JOIN item_totals_on_worst its ON TRUE
 LEFT JOIN items it ON it.id = its.item_id;
 
 
+--Peak Sales Day: select top 10 sums of order total grouped by day in descending order by order total
+SELECT (day_item_total+day_modification_total) as day_order_total from ((select sum(price) as day_item_total from Past_Items group by order_date) inner join (select sum(price) as day_modification_total from Past_Modifications group by order_date));
 
