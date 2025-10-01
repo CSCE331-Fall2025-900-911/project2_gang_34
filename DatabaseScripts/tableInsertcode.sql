dbda43cc5f701df10c63e3decf3f436a33ecc6e8
sowmithb <126096021+sowmithb@users.noreply.github.com>
2025-10-01 08:42:37 -0500

    Sql script sowmith (#2)
    * created two folders and sql code
    
    * first four tables
    
    * first four tables + copy

--- DatabaseScripts/tableInsertcode.sql
+++ DatabaseScripts/tableInsertcode.sql
@@ -1,13 +1,15 @@
--- DROP TABLE Items;
+DROP TABLE Items;
 
--- CREATE TABLE Items (
---     ID INT primary key,
---     modifcation_name TEXT,
---     cost FLOAT,
---     stock INT,
---     active BOOLEAN
+CREATE TABLE Items (
+    ID INT primary key,
+    item_name TEXT,
+    cost FLOAT,
+    stock INT,
+    active BOOLEAN
     
--- );
+);
+
+\copy Items FROM '../GeneratedData/Items.csv' DELIMITER ',' CSV HEADER;
 
 -- INSERT INTO Items
 -- VALUES
@@ -25,12 +27,39 @@ CREATE TABLE Current_Items (
     custom_note TEXT
     
 );
+\copy Items FROM '../GeneratedData/Current_Items.csv' DELIMITER ',' CSV HEADER;
 
 -- INSERT INTO CurrentItems
 -- VALUES
 -- (00,'Latte', 5.99, 1, '1');
 
-SELECT * FROM Current_Items;
+-- SELECT * FROM Current_Items;
+
+DROP TABLE Modifications;
+
+CREATE TABLE Modifications (
+    ID INT primary key,
+    modificaion_name TEXT,
+    item_ID INT,
+    cost FLOAT,
+    stock INT,
+    mod_type TEXT,
+    active BOOLEAN 
+    
+);
+\copy Items FROM '../GeneratedData/Modifications.csv' DELIMITER ',' CSV HEADER;
+
+
+DROP TABLE CurrentModifications;
+
+CREATE TABLE Current_Modifications (
+    ID INT primary key,
+    current_item_ID INT,
+    modification_ID INT,
+    custom_cost FLOAT
+    
+);
+\copy Items FROM '../GeneratedData/Current_Modifications.csv' DELIMITER ',' CSV HEADER;
 
 CREATE TABLE Suggested_Restocks (
     ID INT primary key,
