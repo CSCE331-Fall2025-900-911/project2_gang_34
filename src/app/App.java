c87fb7032c7f228b3f047bd83eeef73bbec8eb3e
pranjol11 <pranjoldas2004@gmail.com>
2025-10-03 08:49:14 -0500

    pushing basic fxml (working!)

--- src/app/App.java
+++ src/app/App.java
@@ -1,3 +1,23 @@
-public class App {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception{
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Project_2.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+
+        stage.setTitle("Gang 34 GUI");
+        stage.setScene(scene);
+        stage.show();
+    }
     
+    public static void main(String[] args) {
+        launch();
+    }
 }
